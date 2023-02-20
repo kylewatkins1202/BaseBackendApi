05fad4b35e7d293b3667da9fc7edc7c5716f5441
kylewatkins1202 <59483260+kylewatkins1202@users.noreply.github.com>
2023-02-20 15:29:46 -0500

    Refresh tokens

--- src/modules/Auth0/index.js
+++ src/modules/Auth0/index.js
@@ -22,7 +22,30 @@ router.get('/login', asyncHandler(async (req, res) => {
         log.error(result)
 
         // Throw error
-        res.status(403).send(`Reason: ${result}`);
+        throw `Bad Request`
+    }
+}))
+
+router.post('/refresh', asyncHandler(async (req, res) => {
+    // Get code from query
+    var refreshToken = req.body.refreshToken
+    
+    // Get result from controller
+    let result = await auth0Controller.refreshToken({
+        refreshToken: refreshToken
+    })
+
+    // If success
+    if (result !== null) {
+        // Send body
+        res.send(result)
+    }
+    else {
+        // Log error
+        log.error(result)
+
+        // Throw error
+        throw `Bad Request`
     }
 }))
 
