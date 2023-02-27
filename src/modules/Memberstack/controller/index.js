ccc4eb5a095790be4a25198981c08f12b824f4f0
kylewatkins1202 <59483260+kylewatkins1202@users.noreply.github.com>
2023-02-27 14:09:07 -0500

    Memberstack flow

--- src/modules/Memberstack/controller/index.js
+++ src/modules/Memberstack/controller/index.js
@@ -2,6 +2,12 @@ const memberstackAdmin = require("@memberstack/admin")
 
 const memberstack = memberstackAdmin.init(process.env.MEMBERSTACKSECRET)
 
+/**
+ * Verify token
+ * @param {object} params Verify token options
+ * @param {string} params.token Token to verify 
+ * @returns True if token is verified false if otherwise
+ */
 const verifyToken = async ({token}) => {
     try {
         let res = await memberstack.verifyToken({token: token})
--- src/modules/Memberstack/middleware/isAuthorized.js
+++ src/modules/Memberstack/middleware/isAuthorized.js
@@ -1,9 +1,29 @@
 const {verifyToken} = require('../controller/index')
 const log = require('npmlog')
+
+/**
+ * Get cookie
+ * @param {string} cookies String of cookies 
+ * @param {string} name Name of cookies to get 
+ * @returns Cookie w.r.t. name
+ */
+function getCookie(cookies, name) {
+    const value = `; ${cookies}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
+/**
+ * Check Token
+ * @param {object} req Request Object
+ * @param {object} res Response Object 
+ * @param {function} next Next middleWare
+ */
 const checkToken = async (req, res, next) => {
     let headers = req.headers
-    let token = (headers?.authorization ?? '').replace('Bearer ', '')
+    const token = getCookie(headers.cookie, '_ms-mid')
     let response = await verifyToken({token: token})
+
     if (response === true) {
         next()
     }
@@ -13,6 +33,13 @@ const checkToken = async (req, res, next) => {
     }
 }
 
+/**
+ * Auth fail catcher
+ * @param {object} error Request Error Object 
+ * @param {object} req Request Object
+ * @param {object} res Response Object 
+ * @param {function} next Next middleWare
+ */
 const authFailCatcher = (error, req, res, next) => {
     if (error !== undefined) {
         // Log error
